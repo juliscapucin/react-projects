4ee04d9f466ff58f63851861b10cee8d60a5120e
juliscapucin <jscapucin@gmail.com>
2022-07-26 17:02:45 +0200

    Refactored time count

---
+++ src/pages/Countdown/Form.js
@@ -0,0 +1,100 @@
+import { useState, useRef, useEffect } from "react";
+
+import Check from "./Check";
+
+export default function Form({
+  date,
+  setDate,
+  today,
+  years,
+  months,
+  setMonths,
+  days,
+  setDays,
+  hours,
+  setHours,
+  minutes,
+  setMinutes,
+  setCheckSubmit,
+}) {
+  const refDate = useRef(null);
+  const refSubmit = useRef(null);
+
+  const [checkDate, setCheckDate] = useState(false);
+
+  useEffect(() => {
+    refSubmit.current.style.display = "none";
+  }, [refSubmit]);
+
+  const validateDate = () => {
+    if (date <= today) {
+      setCheckDate(false);
+      refDate.current.querySelector("span").innerHTML =
+        "A date in the future is required";
+      return false;
+    } else {
+      refDate.current.querySelector("span").innerHTML = "";
+      setCheckDate(true);
+      setTimeout(() => {
+        setCheckDate(false);
+      }, 3000);
+      return true;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateDate()) {
+      refSubmit.current.innerHTML = "Please fix error to submit";
+      refSubmit.current.style.display = "block";
+      setTimeout(() => {
+        setCheckSubmit(false);
+        refSubmit.current.style.display = "none";
+      }, 3000);
+      return false;
+    } else {
+      refSubmit.current.innerHTML = `New date is ${date}`;
+      refSubmit.current.style.display = "block";
+      setTimeout(() => {
+        setCheckSubmit(true);
+        refSubmit.current.style.display = "none";
+      }, 3000);
+    }
+  };
+
+  return (
+    <section className='form__container'>
+      <div className='form__title__container'>
+        <h3>Set a date </h3>
+        <h3>in the future</h3>
+      </div>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className='input__group' ref={refDate}>
+          <label>Day</label>
+          <input
+            type='datetime-local'
+            placeholder=''
+            onChange={(e) => {
+              setDate(new Date(e.target.value));
+            }}
+            min={today}
+            max='2022-12-31T23:59'
+            required
+          />
+          <span className='error__name'></span>
+          {checkDate && (
+            <span className='check__name'>
+              <Check />
+            </span>
+          )}
+        </div>
+
+        <button className='form__btn'>
+          <p>SEND</p>
+        </button>
+        <span ref={refSubmit}></span>
+      </form>
+    </section>
+  );
+}
--- src/pages/Countdown/index.js
+++ src/pages/Countdown/index.js
@@ -1,14 +1,45 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import data from "./data";
+// import data from "./data";
+
+import Form from "./Form";
 
 export default function Countdown() {
-  const [days, setDays] = useState(1);
-  const [hours, setHours] = useState(1);
-  const [minutes, setMinutes] = useState(1);
-  const [seconds, setSeconds] = useState(1);
+  const [date, setDate] = useState(new Date());
+  const [today] = useState(new Date());
+  const [difference, setDifference] = useState(0);
+  const [years, setYears] = useState(0);
+  const [months, setMonths] = useState(0);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [checkSubmit, setCheckSubmit] = useState(false);
+
+  const formatNumber = (n) => {
+    if (n < 10) {
+      return (n = `0${n}`);
+    }
+    return n;
+  };
 
   useEffect(() => {
+    if (date !== today) {
+      setDifference(date.getTime() - today.getTime());
+      const oneDay = 24 * 60 * 60 * 1000;
+      const oneHour = 60 * 60 * 1000;
+      const oneMinute = 60 * 1000;
+
+      setDays(formatNumber(Math.floor(difference / oneDay)));
+      setHours(formatNumber(Math.floor((difference % oneDay) / oneHour)));
+      setMinutes(formatNumber(Math.floor((difference % oneHour) / oneMinute)));
+      setSeconds(formatNumber(Math.floor((difference % oneMinute) / 1000)));
+    }
+  }, [date]);
+
+  useEffect(() => {
+    if (!checkSubmit) return;
+
     const timer = setTimeout(() => {
       if (seconds > 0) {
         setSeconds(seconds - 1);
@@ -57,6 +88,22 @@ export default function Countdown() {
           <h4>Seconds</h4>
         </div>
       </div>
+      <Form
+        date={date}
+        today={today}
+        setDate={setDate}
+        years={years}
+        setYears={setYears}
+        months={months}
+        setMonths={setMonths}
+        days={days}
+        setDays={setDays}
+        hours={hours}
+        setHours={setHours}
+        minutes={minutes}
+        setMinutes={setMinutes}
+        setCheckSubmit={setCheckSubmit}
+      />
     </main>
   );
 }
